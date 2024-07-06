e886435bb692e8bdbf73418930828fd3599b46db
Peter <peteratkinson1994@gmail.com>
2024-07-06 22:43:10 +0100

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tsConfigPaths(),
+  ],
+  build: {
+    emptyOutDir: true,
+    sourcemap: true,
+    copyPublicDir: false,
+    lib: {
+      entry: path.resolve(__dirname, "lib/index.ts"),
+      name: "@rwxpeter/ui",
+      fileName: (format: string) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "react-jsx-runtime",
+        },
+      },
+    },
+  },
+});
